a0356371a60e4611542096e31a176fcdd313d3d9
Gaurav Mane <67306738+Gauravmane16@users.noreply.github.com>
2023-10-19 18:04:03 +0530

    Update Sample 3.py

--- Extra 1/Extra 2/Sample 3.py
+++ Extra 1/Extra 2/Sample 3.py
@@ -1,42 +1,11 @@
-# Old-style Python code
-import random
+#include <iostream>
 
-def GuessTheNumber():
-    print("Welcome to the Number Guessing Game!")
-    number = random.randint(1, 100)#include <iostream.h>
-
-int main()
-{
-    int x;
-    int y;
-    int sum;
-    
-    cout << "Enter the first number: ";
-    cin >> x;
-    
-    cout << "Enter the second number: ";
-    cin >> y;
-    
-    sum = x + y;
-    
-    cout << "The sum of the two numbers is: " << sum;
-    
-    return 0;
+int addNumbers(int x, int y) {
+    return x - y;  // This is the logical error
 }
 
-    attempts = 0
-
-    while True:
-        guess = int(input("Take a guess: "))
-        attempts += 1
-
-        if guess < number:
-            print("Too low! Try again.")
-        elif guess > number:
-            print("Too high! Try again.")
-        else:
-            print("Congratulations! You guessed the number in", attempts, "attempts.")
-            break
-
-if __name__ == "__main__":
-    GuessTheNumber()
+int main() {
+    int result = addNumbers(5, 3);
+    std::cout << "Result: " << result << std::endl;
+    return 0;
+}
